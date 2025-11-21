2423a5e7eaf24fe5d5a5c6ddf4675bfa042b9917
Alexander Harding <AHardin1@seekwell.com>
2025-11-20 17:05:44 -0700

    Added inline documentation for all public symbols

--- pipe.ts
+++ pipe.ts
@@ -1,40 +1,66 @@
 /**
- * Pipe a value through a series of functions.
- * @param fns - The functions to pipe the value through.
- * @returns A function that pipes the value through the functions.
+ * @returns A function that takes in one value and returns it.
  */
 export function pipe<Value>(): (value: Value) => Value;
-export function pipe<A, B>(fn1: (value: A) => B): (value: A) => B;
+/**
+ * Setting up a pipeline of functions.
+ * @returns A function that takes in one value and pipes it through the {@linkcode fn}.
+ */
+export function pipe<A, B>(fn: (value: A) => B): (value: A) => B;
+/**
+ * Setting up a pipeline of functions.
+ * @returns A function that takes in one value and pipes it through the provided functions.
+ */
 export function pipe<A, B, C>(
   fn1: (value: A) => B,
-  fn2: (value: B) => C,
+  fn2: (value: B) => C
 ): (value: A) => C;
+/**
+ * Setting up a pipeline of functions.
+ * @returns A function that takes in one value and pipes it through the provided functions.
+ */
 export function pipe<A, B, C, D>(
   fn1: (value: A) => B,
   fn2: (value: B) => C,
-  fn3: (value: C) => D,
+  fn3: (value: C) => D
 ): (value: A) => D;
+/**
+ * Setting up a pipeline of functions.
+ * @returns A function that takes in one value and pipes it through the provided functions.
+ */
 export function pipe<A, B, C, D, E>(
   fn1: (value: A) => B,
   fn2: (value: B) => C,
   fn3: (value: C) => D,
-  fn4: (value: D) => E,
+  fn4: (value: D) => E
 ): (value: A) => E;
+/**
+ * Setting up a pipeline of functions.
+ * @returns A function that takes in one value and pipes it through the provided functions.
+ */
 export function pipe<A, B, C, D, E, F>(
   fn1: (value: A) => B,
   fn2: (value: B) => C,
   fn3: (value: C) => D,
   fn4: (value: D) => E,
-  fn5: (value: E) => F,
+  fn5: (value: E) => F
 ): (value: A) => F;
+/**
+ * Setting up a pipeline of functions.
+ * @returns A function that takes in one value and pipes it through the provided functions.
+ */
 export function pipe<A, B, C, D, E, F, G>(
   fn1: (value: A) => B,
   fn2: (value: B) => C,
   fn3: (value: C) => D,
   fn4: (value: D) => E,
   fn5: (value: E) => F,
-  fn6: (value: F) => G,
+  fn6: (value: F) => G
 ): (value: A) => G;
+/**
+ * Setting up a pipeline of functions.
+ * @returns A function that takes in one value and pipes it through the provided functions.
+ */
 export function pipe<A, B, C, D, E, F, G, H>(
   fn1: (value: A) => B,
   fn2: (value: B) => C,
@@ -42,8 +68,12 @@ export function pipe<A, B, C, D, E, F, G, H>(
   fn4: (value: D) => E,
   fn5: (value: E) => F,
   fn6: (value: F) => G,
-  fn7: (value: G) => H,
+  fn7: (value: G) => H
 ): (value: A) => H;
+/**
+ * Setting up a pipeline of functions.
+ * @returns A function that takes in one value and pipes it through the provided functions.
+ */
 export function pipe<A, B, C, D, E, F, G, H, I>(
   fn1: (value: A) => B,
   fn2: (value: B) => C,
@@ -52,8 +82,12 @@ export function pipe<A, B, C, D, E, F, G, H, I>(
   fn5: (value: E) => F,
   fn6: (value: F) => G,
   fn7: (value: G) => H,
-  fn8: (value: H) => I,
+  fn8: (value: H) => I
 ): (value: A) => I;
+/**
+ * Setting up a pipeline of functions.
+ * @returns A function that takes in one value and pipes it through the provided functions.
+ */
 export function pipe<A, B, C, D, E, F, G, H, I, J>(
   fn1: (value: A) => B,
   fn2: (value: B) => C,
@@ -63,8 +97,12 @@ export function pipe<A, B, C, D, E, F, G, H, I, J>(
   fn6: (value: F) => G,
   fn7: (value: G) => H,
   fn8: (value: H) => I,
-  fn9: (value: I) => J,
+  fn9: (value: I) => J
 ): (value: A) => J;
+/**
+ * Setting up a pipeline of functions.
+ * @returns A function that takes in one value and pipes it through the provided functions.
+ */
 export function pipe<A, B, C, D, E, F, G, H, I, J, K>(
   fn1: (value: A) => B,
   fn2: (value: B) => C,
@@ -75,8 +113,12 @@ export function pipe<A, B, C, D, E, F, G, H, I, J, K>(
   fn7: (value: G) => H,
   fn8: (value: H) => I,
   fn9: (value: I) => J,
-  fn10: (value: J) => K,
+  fn10: (value: J) => K
 ): (value: A) => K;
+/**
+ * Setting up a pipeline of functions.
+ * @returns A function that takes in one value and pipes it through the provided functions.
+ */
 export function pipe<A, B, C, D, E, F, G, H, I, J, K, L>(
   fn1: (value: A) => B,
   fn2: (value: B) => C,
@@ -88,8 +130,12 @@ export function pipe<A, B, C, D, E, F, G, H, I, J, K, L>(
   fn8: (value: H) => I,
   fn9: (value: I) => J,
   fn10: (value: J) => K,
-  fn11: (value: K) => L,
+  fn11: (value: K) => L
 ): (value: A) => L;
+/**
+ * Setting up a pipeline of functions.
+ * @returns A function that takes in one value and pipes it through the provided functions.
+ */
 export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N>(
   fn1: (value: A) => B,
   fn2: (value: B) => C,
@@ -103,8 +149,12 @@ export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N>(
   fn10: (value: J) => K,
   fn11: (value: K) => L,
   fn12: (value: L) => M,
-  fn13: (value: M) => N,
+  fn13: (value: M) => N
 ): (value: A) => N;
+/**
+ * Setting up a pipeline of functions.
+ * @returns A function that takes in one value and pipes it through the provided functions.
+ */
 export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O>(
   fn1: (value: A) => B,
   fn2: (value: B) => C,
@@ -119,9 +169,12 @@ export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O>(
   fn11: (value: K) => L,
   fn12: (value: L) => M,
   fn13: (value: M) => N,
-  fn14: (value: N) => O,
+  fn14: (value: N) => O
 ): (value: A) => O;
-
+/**
+ * Setting up a pipeline of functions.
+ * @returns A function that takes in one value and pipes it through the provided functions.
+ */
 export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P>(
   fn1: (value: A) => B,
   fn2: (value: B) => C,
@@ -137,8 +190,12 @@ export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P>(
   fn12: (value: L) => M,
   fn13: (value: M) => N,
   fn14: (value: N) => O,
-  fn15: (value: O) => P,
+  fn15: (value: O) => P
 ): (value: A) => P;
+/**
+ * Setting up a pipeline of functions.
+ * @returns A function that takes in one value and pipes it through the provided functions.
+ */
 export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q>(
   fn1: (value: A) => B,
   fn2: (value: B) => C,
@@ -155,8 +212,12 @@ export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q>(
   fn13: (value: M) => N,
   fn14: (value: N) => O,
   fn15: (value: O) => P,
-  fn16: (value: P) => Q,
-): unknown;
+  fn16: (value: P) => Q
+): (value: A) => Q;
+/**
+ * Setting up a pipeline of functions.
+ * @returns A function that takes in one value and pipes it through the provided functions.
+ */
 export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q>(
   fn1: (value: A) => B,
   fn2: (value: B) => C,
--- rx.ts
+++ rx.ts
@@ -1,39 +1,69 @@
 import { pipe } from "./pipe.ts";
 
 /**
- * Pipe a {@linkcode source|value} through a series of unary functions.
- * @param source - The {@linkcode source|value} to pipe.
- * @param fns - The functions to pipe the {@linkcode source|value} through.
- * @returns The result of the last function.
+ * A unary function that takes a {@linkcode source|value} and returns it.
+ * @param source - The {@linkcode source|value} to return.
+ * @returns The {@linkcode source|value}.
  */
 export function rx<Value>(source: Value): Value;
+/**
+ * Pipe a {@linkcode source|value} through a {@linkcode fn}.
+ * @param source - The {@linkcode source|value} to pipe.
+ * @param fn - The {@linkcode fn} to pipe the {@linkcode source|value} through.
+ * @returns The result of the {@linkcode fn}.
+ */
 export function rx<In, Out>(source: In, fn: (value: In) => Out): Out;
+/**
+ * Pipe a {@linkcode source|value} through a series of unary functions.
+ * @param source - The {@linkcode source|value} to pipe.
+ * @returns The result of the {@linkcode fn2}.
+ */
 export function rx<In, A, B>(
   source: In,
   fn1: (value: In) => A,
-  fn2: (value: A) => B,
+  fn2: (value: A) => B
 ): B;
+/**
+ * Pipe a {@linkcode source|value} through a series of unary functions.
+ * @param source - The {@linkcode source|value} to pipe.
+ * @returns The result of the {@linkcode fn3}.
+ */
 export function rx<In, A, B, C>(
   source: In,
   fn1: (value: In) => A,
   fn2: (value: A) => B,
-  fn3: (value: B) => C,
+  fn3: (value: B) => C
 ): C;
+/**
+ * Pipe a {@linkcode source|value} through a series of unary functions.
+ * @param source - The {@linkcode source|value} to pipe.
+ * @returns The result of the {@linkcode fn4}.
+ */
 export function rx<In, A, B, C, D>(
   source: In,
   fn1: (value: In) => A,
   fn2: (value: A) => B,
   fn3: (value: B) => C,
-  fn4: (value: C) => D,
+  fn4: (value: C) => D
 ): D;
+/**
+ * Pipe a {@linkcode source|value} through a series of unary functions.
+ * @param source - The {@linkcode source|value} to pipe.
+ * @returns The result of the {@linkcode fn5}.
+ */
 export function rx<In, A, B, C, D, E>(
   source: In,
   fn1: (value: In) => A,
   fn2: (value: A) => B,
   fn3: (value: B) => C,
   fn4: (value: C) => D,
-  fn5: (value: D) => E,
+  fn5: (value: D) => E
 ): E;
+/**
+ * Pipe a {@linkcode source|value} through a series of unary functions.
+ * @param source - The {@linkcode source|value} to pipe.
+ * @returns The result of the {@linkcode fn6}.
+ */
 export function rx<In, A, B, C, D, E, F>(
   source: In,
   fn1: (value: In) => A,
@@ -41,8 +71,13 @@ export function rx<In, A, B, C, D, E, F>(
   fn3: (value: B) => C,
   fn4: (value: C) => D,
   fn5: (value: D) => E,
-  fn6: (value: E) => F,
+  fn6: (value: E) => F
 ): F;
+/**
+ * Pipe a {@linkcode source|value} through a series of unary functions.
+ * @param source - The {@linkcode source|value} to pipe.
+ * @returns The result of the {@linkcode fn7}.
+ */
 export function rx<In, A, B, C, D, E, F, G>(
   source: In,
   fn1: (value: In) => A,
@@ -51,8 +86,13 @@ export function rx<In, A, B, C, D, E, F, G>(
   fn4: (value: C) => D,
   fn5: (value: D) => E,
   fn6: (value: E) => F,
-  fn7: (value: F) => G,
+  fn7: (value: F) => G
 ): G;
+/**
+ * Pipe a {@linkcode source|value} through a series of unary functions.
+ * @param source - The {@linkcode source|value} to pipe.
+ * @returns The result of the {@linkcode fn8}.
+ */
 export function rx<In, A, B, C, D, E, F, G, H>(
   source: In,
   fn1: (value: In) => A,
@@ -62,8 +102,13 @@ export function rx<In, A, B, C, D, E, F, G, H>(
   fn5: (value: D) => E,
   fn6: (value: E) => F,
   fn7: (value: F) => G,
-  fn8: (value: G) => H,
+  fn8: (value: G) => H
 ): H;
+/**
+ * Pipe a {@linkcode source|value} through a series of unary functions.
+ * @param source - The {@linkcode source|value} to pipe.
+ * @returns The result of the {@linkcode fn9}.
+ */
 export function rx<In, A, B, C, D, E, F, G, H, I>(
   source: In,
   fn1: (value: In) => A,
@@ -74,8 +119,13 @@ export function rx<In, A, B, C, D, E, F, G, H, I>(
   fn6: (value: E) => F,
   fn7: (value: F) => G,
   fn8: (value: G) => H,
-  fn9: (value: H) => I,
+  fn9: (value: H) => I
 ): I;
+/**
+ * Pipe a {@linkcode source|value} through a series of unary functions.
+ * @param source - The {@linkcode source|value} to pipe.
+ * @returns The result of the {@linkcode fn10}.
+ */
 export function rx<In, A, B, C, D, E, F, G, H, I, J>(
   source: In,
   fn1: (value: In) => A,
@@ -87,8 +137,13 @@ export function rx<In, A, B, C, D, E, F, G, H, I, J>(
   fn7: (value: F) => G,
   fn8: (value: G) => H,
   fn9: (value: H) => I,
-  fn10: (value: I) => J,
+  fn10: (value: I) => J
 ): J;
+/**
+ * Pipe a {@linkcode source|value} through a series of unary functions.
+ * @param source - The {@linkcode source|value} to pipe.
+ * @returns The result of the {@linkcode fn11}.
+ */
 export function rx<In, A, B, C, D, E, F, G, H, I, J, K>(
   source: In,
   fn1: (value: In) => A,
@@ -101,8 +156,13 @@ export function rx<In, A, B, C, D, E, F, G, H, I, J, K>(
   fn8: (value: G) => H,
   fn9: (value: H) => I,
   fn10: (value: I) => J,
-  fn11: (value: J) => K,
+  fn11: (value: J) => K
 ): K;
+/**
+ * Pipe a {@linkcode source|value} through a series of unary functions.
+ * @param source - The {@linkcode source|value} to pipe.
+ * @returns The result of the {@linkcode fn12}.
+ */
 export function rx<In, A, B, C, D, E, F, G, H, I, J, K, L>(
   source: In,
   fn1: (value: In) => A,
@@ -116,8 +176,13 @@ export function rx<In, A, B, C, D, E, F, G, H, I, J, K, L>(
   fn9: (value: H) => I,
   fn10: (value: I) => J,
   fn11: (value: J) => K,
-  fn12: (value: K) => L,
+  fn12: (value: K) => L
 ): L;
+/**
+ * Pipe a {@linkcode source|value} through a series of unary functions.
+ * @param source - The {@linkcode source|value} to pipe.
+ * @returns The result of the {@linkcode fn13}.
+ */
 export function rx<In, A, B, C, D, E, F, G, H, I, J, K, L, M>(
   source: In,
   fn1: (value: In) => A,
@@ -132,8 +197,13 @@ export function rx<In, A, B, C, D, E, F, G, H, I, J, K, L, M>(
   fn10: (value: I) => J,
   fn11: (value: J) => K,
   fn12: (value: K) => L,
-  fn13: (value: L) => M,
+  fn13: (value: L) => M
 ): M;
+/**
+ * Pipe a {@linkcode source|value} through a series of unary functions.
+ * @param source - The {@linkcode source|value} to pipe.
+ * @returns The result of the {@linkcode fn14}.
+ */
 export function rx<In, A, B, C, D, E, F, G, H, I, J, K, L, M, N>(
   source: In,
   fn1: (value: In) => A,
@@ -149,8 +219,13 @@ export function rx<In, A, B, C, D, E, F, G, H, I, J, K, L, M, N>(
   fn11: (value: J) => K,
   fn12: (value: K) => L,
   fn13: (value: L) => M,
-  fn14: (value: M) => N,
+  fn14: (value: M) => N
 ): N;
+/**
+ * Pipe a {@linkcode source|value} through a series of unary functions.
+ * @param source - The {@linkcode source|value} to pipe.
+ * @returns The result of the {@linkcode fn15}.
+ */
 export function rx<In, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O>(
   source: In,
   fn1: (value: In) => A,
@@ -167,8 +242,13 @@ export function rx<In, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O>(
   fn12: (value: K) => L,
   fn13: (value: L) => M,
   fn14: (value: M) => N,
-  fn15: (value: N) => O,
+  fn15: (value: N) => O
 ): O;
+/**
+ * Pipe a {@linkcode source|value} through a series of unary functions.
+ * @param source - The {@linkcode source|value} to pipe.
+ * @returns The result of the {@linkcode fn16}.
+ */
 export function rx<In, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P>(
   source: In,
   fn1: (value: In) => A,
@@ -186,8 +266,13 @@ export function rx<In, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P>(
   fn13: (value: L) => M,
   fn14: (value: M) => N,
   fn15: (value: N) => O,
-  fn16: (value: O) => P,
+  fn16: (value: O) => P
 ): P;
+/**
+ * Pipe a {@linkcode source|value} through a series of unary functions.
+ * @param source - The {@linkcode source|value} to pipe.
+ * @returns The result of the last {@linkcode fns|fn}.
+ */
 export function rx<In, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P>(
   source: In,
   fn1: (value: In) => A,
@@ -207,7 +292,7 @@ export function rx<In, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P>(
   fn15: (value: N) => O,
   fn16: (value: O) => P,
   ...fns: ReadonlyArray<(value: P) => unknown>
-): P;
+): unknown;
 export function rx(source: unknown, ...fns: []): unknown {
   return pipe(...fns)(source);
 }
